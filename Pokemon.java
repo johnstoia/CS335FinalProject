0b4c23914ee7b73d22f1e3f5ff99850abb0fbb31
John Stoia <johnstoia@email.arizona.edu>
2015-04-27 23:55:13 -0700

    Updated Pokemon Class. With main for testing (will be taken out soon)

--- Pokemon.java
+++ Pokemon.java
@@ -1,26 +1,51 @@
-import java.lang.Math;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Random;
 
-public abstract class Pokemon {
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Pokemon {
 	
 	private String name;
 	private int rarity;
 	private int likelyToRun;
 	private int likelyToCatch;
+	private BufferedImage image;
 	
 	/**
 	 * Description: 
 	 *
-	 * Pokemon Constructor
+	 * Pokemon Constructor. Pass in a name and a rarity and the rest is calculated
 	 * 
 	 * @param String name, Int rarity, Int toRun, Int toCatch
 	 * @return
 	 * @author John Stoia
 	 */
-	public Pokemon(String name, int rarity, int toRun, int toCatch){
+	public Pokemon(String name, int rarity){
 		this.name = name;
 		this.rarity = rarity;
-		this.likelyToRun = toRun;
-		this.likelyToCatch = toCatch;
+		if(this.image == null){
+			try{
+				this.image = ImageIO.read(new File("images/"+name+".png"));
+			}catch(Exception e) {
+				System.out.println("No Picture Found!");
+			};
+		
+		this.likelyToRun = 30;
+		
+		if(rarity >= 1 && rarity <= 6) this.likelyToCatch = 40;
+		if(rarity > 6 && rarity <= 9) this.likelyToCatch = 30;
+		if(rarity == 10) this.likelyToCatch = 20;
+		
+	}
+
+		
 		
 	}
 	
@@ -76,6 +101,10 @@ public abstract class Pokemon {
 		return likelyToCatch;
 	}
 	
+	public Image getImage(){
+		return image;
+	}
+	
 	/**
 	 * Description: 
 	 *
@@ -86,7 +115,7 @@ public abstract class Pokemon {
 	 * @author John Stoia
 	 */
 	public void upRun(){
-		
+			likelyToRun += 10;
 	}
 	
 	/**
@@ -100,6 +129,7 @@ public abstract class Pokemon {
 	 */
 	
 	public void downRun(){
+		likelyToRun -= 10;
 		
 	}
 	
@@ -113,7 +143,7 @@ public abstract class Pokemon {
 	 * @author John Stoia
 	 */
 	public void upCatch(){
-		
+		likelyToCatch += 10;
 	}
 	
 	/**
@@ -126,7 +156,7 @@ public abstract class Pokemon {
 	 * @author John Stoia
 	 */
 	public void downCatch(){
-		
+		likelyToCatch -= 10;
 	}
 	
 	/**
@@ -140,7 +170,16 @@ public abstract class Pokemon {
 	 */
 	
 	public boolean catches(){
-		return false;
+		Random r = new Random();
+		int low = 1;
+		int high = 100;
+		int randomNumber = r.nextInt(high-low) + low;
+		
+		if(randomNumber <= likelyToCatch){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	/**
@@ -153,6 +192,37 @@ public abstract class Pokemon {
 	 * @author John Stoia
 	 */
 	public boolean runs(){
-		return false;
+		Random r = new Random();
+		int low = 1;
+		int high = 100;
+		int randomNumber = r.nextInt(high-low) + low;
+		
+		System.out.println(randomNumber);
+		if(randomNumber <= likelyToCatch){
+			return true;
+		}else{
+			return false;
+		}
 	}
-}
+	
+	public static void main(String []args){
+	Pokemon gregachu = new Pokemon("gregachu",9);
+	Graphics g;
+	JPanel panel = new JPanel(){
+		private static final long serialVersionUID = 1L;
+
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.drawImage(gregachu.getImage(),0,0,300,400,null);
+	}};
+	
+	JFrame x = new JFrame();
+	x.setSize(new Dimension(300,450));
+	x.add(panel);
+	x.setVisible(true);
+	
+	System.out.println(gregachu.runs());
+	}
+	}
+	
+
