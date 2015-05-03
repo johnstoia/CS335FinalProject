6e175a5c857e19d274459c37abbac79f93697c4f
John Stoia <johnstoia@email.arizona.edu>
2015-05-02 18:11:06 -0700

    Full Pokemon model source including Classes Items(PokeBall,SafariBall,MasterBall,RunningShoes,SleepDart), Pokemon, Trainer, PokeBattle, PokeWorld

---
+++ Pokemon/src/SafariBall.java
@@ -0,0 +1,52 @@
+
+
+public class SafariBall extends Pokeball {
+	private String itemName;
+	private int quantity;
+	private double catchRate;
+	
+	/**
+	 * Description: 
+	 *
+	 * SafariBall Constructor
+	 * 
+	 * @param String itemName, int quantity
+	 * @return
+	 * @author Christopher Leon
+	 */
+	public SafariBall( String itemName, int quantity){
+		super(itemName, quantity);
+	}
+	
+	/**
+	 * Description: 
+	 *
+	 * useItem, method to use an item
+	 * 
+	 * @param 
+	 * @return String
+	 * @author Christopher Leon & John Stoia
+	 */
+	public String useItem(){
+		if( this.getQuantity() == 0){
+			return "You do not have any " +this.itemName + "s left!";
+		}
+		else{
+			quantity--;
+			return "You Threw a Safari Ball!";
+		}
+	}
+	
+	/**
+	 * Description: 
+	 *
+	 * getQuantity, method to find out how many of an item the player has
+	 * 
+	 * @param 
+	 * @return int
+	 * @author Christopher Leon & John Stoia
+	 */
+	public int getQuantity(){
+		return quantity;
+		}
+}
